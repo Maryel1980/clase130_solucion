7791d2fd1e31dc353c37b6cb0664cad468c9583f
Maryel1980 <87047908+Maryel1980@users.noreply.github.com>
2022-12-14 16:51:25 -0600

    Add files via upload

---
+++ PRO_C130_Plantilla_del_codigo_V2-main/main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+#Leer el archivo csv.
+df = pd.read_csv("final.csv")
+#Verificar el número de filas y columnas.
+print(df.shape)
+
+#Borrar columnas.
+
+#Renombrar encabezados de las columnas.
+
+#Guardar los datos en main.csv.
